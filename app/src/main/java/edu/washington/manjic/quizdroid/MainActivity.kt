4baec01eea1536d74d8276077babcb8bb74ab4fd
cchen97 <manjic@uw.edu>
2019-05-09 14:02:44 -0700

    ???

--- app/src/main/java/edu/washington/manjic/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/manjic/quizdroid/MainActivity.kt
@@ -6,9 +6,10 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import edu.washington.manjic.quizdroid.R.id.listView_topics
 import android.content.Intent
-import android.content.SharedPreferences
-import android.content.Context
-
+import android.view.Menu
+import android.view.MenuInflater
+import android.R
+import android.widget.FrameLayout
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,11 +23,11 @@ class MainActivity : AppCompatActivity() {
 
 
         var topicListView = findViewById<ListView>(listView_topics)
-        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, topics)
+        var adapter = ArrayAdapter<String>(this, R.layout.simple_list_item_1, topics)
         topicListView.adapter = adapter
 
         topicListView.setOnItemClickListener { _, _ , position, id ->
-            var currentView: ListView = findViewById(R.id.listView_topics)
+            var currentView: ListView = findViewById(listView_topics)
             var chosenItem = currentView.getItemAtPosition(position)
             val intent = Intent(this, ControllerActivity::class.java)
                 .putExtra(ControllerActivity.TOPIC_NAME, chosenItem.toString())
@@ -34,5 +35,17 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.preference, menu)
+
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        val MenuItem = menu?.findItem(R.id.preferenceContainer)
+        val rootView = MenuItem?.actionView as FrameLayout
+        return super.onPrepareOptionsMenu(menu)
     }
 }
