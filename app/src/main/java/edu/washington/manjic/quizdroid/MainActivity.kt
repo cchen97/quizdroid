5814d773e1cfc8436c44ecc45062f89fb66e2f91
ZKeiLin <zlin2016@uw.edu>
2019-05-09 17:11:46 -0700

    add dialog

--- app/src/main/java/edu/washington/manjic/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/manjic/quizdroid/MainActivity.kt
@@ -1,5 +1,7 @@
 package edu.washington.manjic.quizdroid
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -8,7 +10,11 @@ import edu.washington.manjic.quizdroid.R.id.listView_topics
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.Context
-
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.LinearLayout
 
 
 class MainActivity : AppCompatActivity() {
@@ -32,7 +38,25 @@ class MainActivity : AppCompatActivity() {
                 .putExtra(ControllerActivity.TOPIC_NAME, chosenItem.toString())
             startActivity(intent)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.preference, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val inflater : LayoutInflater = this.getLayoutInflater()
+        val rootView  = inflater.inflate(R.layout.activity_preferences ,null)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Input your own quiz").setView(rootView)
+        builder.show()
 
 
+        return super.onOptionsItemSelected(item)
     }
 }
